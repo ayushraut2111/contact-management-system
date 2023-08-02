8db894228a99915d554275cdf5cf2c2acd9f3b30
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-08-02 10:26:07 +0530

    minor changes

--- contact_management/contacts/models.py
+++ contact_management/contacts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class contacts(models.Model):
     name=models.CharField(max_length=100)
-    phone=models.CharField(primary_key=True,max_length=10)
+    phone=models.CharField(primary_key=True,max_length=20)
     email=models.EmailField(max_length=100)
     address=models.TextField(max_length=255)
 
